14194adc036ccddbf65847df8fe260a118f99e86
HassanMohamed-cs <hassanashour639@gmail.com>
2026-03-24 10:21:10 +0200

    Kick-off

---
+++ frontend/src/main.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import routes from './routes'
+const router = createBrowserRouter(routes)
+
+createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />
+);
